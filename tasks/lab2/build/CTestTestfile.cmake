dae5f44dd9eb4c9758ee323207a5b297ebe96302
Ivan Bakholdin <bakholdin.ivan11@gmail.com>
2025-12-13 16:36:15 +0300

    Delete tasks/lab2 directory

--- tasks/lab2/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/labs_OOP/tasks/lab2
-# Build directory: /workspaces/labs_OOP/tasks/lab2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TwelveTests "/workspaces/labs_OOP/tasks/lab2/build/tests")
-set_tests_properties(TwelveTests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/labs_OOP/tasks/lab2/CMakeLists.txt;30;add_test;/workspaces/labs_OOP/tasks/lab2/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
