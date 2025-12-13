74a9a05a06c3152b34090ade345003e429427f49
Ivan Bakholdin <bakholdin.ivan11@gmail.com>
2025-12-13 16:36:22 +0300

    Delete tasks/lab3 directory

--- tasks/lab3/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/labs_OOP/tasks/lab3
-# Build directory: /workspaces/labs_OOP/tasks/lab3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/workspaces/labs_OOP/tasks/lab3/build/tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
