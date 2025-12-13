e69091f3a855f43e7ed8ce4ab5a5a70cea7d8016
Ivan Bakholdin <bakholdin.ivan11@gmail.com>
2025-12-13 16:35:59 +0300

    Delete tasks/lab06 directory

--- tasks/lab06/include/Bull.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "NPC.h"
-
-class Bull : public NPC {
-public:
-    Bull(const std::string& name, double x, double y);
-    void accept(BattleVisitor& visitor, NPC& other) override;
-    std::string getType() const override { return "Bull"; }
-};
