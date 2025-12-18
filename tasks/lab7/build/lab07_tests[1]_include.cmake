1ac3026a8a47a5a67cc83876e0bd88b2d0ffeed8
Ivan Bakholdin <bakholdin.ivan11@gmail.com>
2025-12-18 22:14:42 +0300

    Delete tasks/lab7/build directory

--- tasks/lab7/build/lab07_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/workspaces/labs_OOP/tasks/lab7/build/lab07_tests[1]_tests.cmake")
-  include("/workspaces/labs_OOP/tasks/lab7/build/lab07_tests[1]_tests.cmake")
-else()
-  add_test(lab07_tests_NOT_BUILT lab07_tests_NOT_BUILT)
-endif()
